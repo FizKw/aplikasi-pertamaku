19082d07e16a5917f468171c1d45be871426e02f
FizKw <hafizhkumara.w@gmail.com>
2024-10-24 06:11:10 +0700

    fix: revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hafizh/',
   plugins: [vue()],
   server:{
     watch:{
@@ -12,5 +11,6 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 5173,
-  }
+  },
+
 })
